f3c5336d4344bceaf61641c1271f6d94c31b6259
jaydeep-R <jaydeepsinhrathod01@gmail.com>
2023-01-03 12:06:58 +0530

    project name update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Jaydeep Rathod',
-  tagline: 'full-stack development, product innovation, branding & marketing',
+  tagline: 'full-stack development, platform engineering, product innovation, branding & marketing',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
@@ -17,7 +17,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  projectName: 'portfolio', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
