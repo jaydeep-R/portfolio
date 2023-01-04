6232f3f90c012feb04109dce255779510e152109
jaydeep-R <jaydeepsinhrathod01@gmail.com>
2023-01-04 12:24:01 +0530

    config update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,18 +49,22 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
-      }),
-    ],
-    [
-      '@docusaurus/preset-classic',
-      {
-        
         gtag: {
           trackingID: 'G-4ZJWZX87Z9',
           anonymizeIP: true,
         },
-      },
+      }),
     ],
+    // [
+    //   '@docusaurus/preset-classic',
+    //   {
+        
+    //     gtag: {
+    //       trackingID: 'G-4ZJWZX87Z9',
+    //       anonymizeIP: true,
+    //     },
+    //   },
+    // ],
   ],
 
   themeConfig:
